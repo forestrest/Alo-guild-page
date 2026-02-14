c3ab0ae5004999acc1a31b75a35f36f71e1cdb16
Forestrest <felipegcofdi@gmail.com>
2026-02-14 01:36:54 -0500

    Last changes by Connection DB Railway

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,10 +1,9 @@
 import mysql from "mysql2/promise";
 
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "shuttle.proxy.rlwy.net",
+  user: "root",
+  port: Number("42207"),
+  password: "TLWDKvtWdFBrPQbjJNtZREwcglbslBVR",
+  database: "railway",
 });
-
